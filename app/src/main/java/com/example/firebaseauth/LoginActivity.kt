62f417e45e1c7b3ab3a1794afcc222b392f8bcda
Christinepeng <percussionpeng@gmail.com>
2022-05-09 21:43:42 -0700

    login btn check email.

--- app/src/main/java/com/example/firebaseauth/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseauth/LoginActivity.kt
@@ -2,13 +2,31 @@ package com.example.firebaseauth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val loginBottom: Button = findViewById(R.id.btn_login)
+        val email:EditText = findViewById(R.id.editTextTextEmailAddress)
+        val password:EditText = findViewById(R.id.editTextTextPassword)
+
+        loginBottom.setOnClickListener {
+            when {
+                TextUtils.isEmpty(email.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/firebaseauth/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseauth/RegisterActivity.kt
@@ -1,15 +1,16 @@
 package com.example.firebaseauth
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
 
+    val registerBottom: Button = findViewById(R.id.btn_register)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
 
     }
 }
